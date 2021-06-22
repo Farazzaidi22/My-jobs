ad8f5726730a92762bca35b3b04d9380dcfdcb98
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-06-23 03:46:34 +0500

    created job component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vitae dolores voluptates. Iste pariatur quae recusandae aliquid perferendis iusto porro alias, optio vero nulla harum explicabo laboriosam a facilis quo, temporibus distinctio maiores atque repellat ut vel ducimus. Eos at corrupti dolorem perspiciatis soluta assumenda velit quas explicabo. Laudantium, ut repellat adipisci sed reprehenderit veniam delectus quibusdam numquam vero corporis ab expedita, provident distinctio deserunt rem tempore incidunt! Ratione blanditiis esse ex dolorum est dolore, neque, consequatur molestiae doloribus dolorem voluptatem excepturi facere debitis vitae impedit veritatis sunt recusandae voluptas magnam. Deserunt vitae tempore pariatur aliquam iure quisquam, eos ex.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vitae dolores voluptates. Iste pariatur quae recusandae aliquid perferendis iusto porro alias, optio vero nulla harum explicabo laboriosam a facilis quo, temporibus distinctio maiores atque repellat ut vel ducimus. Eos at corrupti dolorem perspiciatis soluta assumenda velit quas explicabo. Laudantium, ut repellat adipisci sed reprehenderit veniam delectus quibusdam numquam vero corporis ab expedita, provident distinctio deserunt rem tempore incidunt! Ratione blanditiis esse ex dolorum est dolore, neque, consequatur molestiae doloribus dolorem voluptatem excepturi facere debitis vitae impedit veritatis sunt recusandae voluptas magnam. Deserunt vitae tempore pariatur aliquam iure quisquam, eos ex.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h2>{{job.title}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+    data(){
+        return{
+            jobs:[
+                {title: 'UXUI Designer', id: 1, details: 'lorem'},
+                {title: 'Web Developer', id: 1, details: 'lorem'},
+                {title: 'Vue Developer', id: 1, details: 'lorem'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
