3a23ad2a87c6e8af1a0fa00327a6037d1ee99026
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-06-24 03:00:57 +0500

    fetched and displayed data from local json file

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,31 @@
 <template>
-  <h1>Job Details page</h1>
+<div v-if="job">
+  <h1>{{ job.title }}</h1>
   <p>The job id is {{id}}</p>
+  <p>{{ job.details }}</p>
+</div>
+<div v-else>
+  <h1>Loading job details...</h1>
+</div>
 </template>
 
 <script>
 export default {
     props: ['id'],
+
+    data(){
+      return{
+        job: null
+      }
+    },
+
+
+    mounted(){
+        fetch('http://localhost:3000/' + this.id)
+        .then( (res) => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
+    }
     // data(){
     //     return{
     //         id: this.$route.params.id
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,15 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
         <router-link :to="{name: 'JobDetails', params:{id: job.id}}">
         <h2>{{job.title}}</h2>
         </router-link>
+    </div>
+    </div>
+
+    <div v-else>
+        <h1>Loading jobs...</h1>
     </div>
 </template>
 
@@ -13,11 +19,18 @@ export default {
     data(){
         return{
             jobs:[
-                {title: 'UXUI Designer', id: 1, details: 'lorem'},
-                {title: 'Web Developer', id: 2, details: 'lorem'},
-                {title: 'Vue Developer', id: 3, details: 'lorem'},
+                // {title: 'UXUI Designer', id: 1, details: 'lorem'},
+                // {title: 'Web Developer', id: 2, details: 'lorem'},
+                // {title: 'Vue Developer', id: 3, details: 'lorem'},
             ]
         }
+    },
+
+    mounted(){
+        fetch('http://localhost:3000/jobs')
+        .then( (res) => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     }
 }
 </script>
